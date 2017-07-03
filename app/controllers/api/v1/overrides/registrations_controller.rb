87c88bc79c0e7bcf7ada64fb432aa83f10df45dc
Felipe Tovar <f.tovar12@hotmail.com>
2017-07-03 12:19:59 -0500

    Added error handler for orgs that doesnt exist

--- app/controllers/api/v1/overrides/registrations_controller.rb
+++ app/controllers/api/v1/overrides/registrations_controller.rb
@@ -1,10 +1,20 @@
 module Api::V1::Overrides
   class RegistrationsController < DeviseTokenAuth::RegistrationsController
+    before_action :check_orgs_exist, only: [:create, :update]
     after_action :assign_organizations, only: [:create, :update]
 
     protected
     def assign_organizations
-      @resource.organizations = Organization.find(params[:organizations_ids])
+      @resource.organizations = @organizations
+      binding.pry
+    end
+
+    def check_orgs_exist
+      begin
+        @organizations = Organization.find(params[:organizations_ids])
+      rescue ActiveRecord::RecordNotFound
+        render json: { status: 'error', errors: ["One or more organizations ids weren't found"] }
+      end
     end
   end
 end
