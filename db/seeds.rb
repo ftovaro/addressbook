a468cfde2bb044f39bc9e5b5d43b9c3eee99916e
Felipe Tovar <f.tovar12@hotmail.com>
2017-07-03 19:52:11 -0500

    Added seeds for regular and admin users, org and contact

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+admin_user = User.create name: 'Felipe', email: 'felipe@email.com', password: 'password1'
+admin_user.add_role :admin
+regular_user = User.create name: 'Santi', email: 'santi@email.com', password: 'password1'
+regular_user.add_role :regular
+org = Organization.create name: "STRV"
+org.users.push admin_user, regular_user
+org.save
+contact = {
+  full_name: "Felipe Tovar",
+  address: "531 E 3rd St, Brooklyn",
+  phone: "+573117233109",
+  city: 'New York'
+}
+Contact.create_contact contact, org.id
