d64a29c330e5465a4e9ed25ebd8aff10277a8a09
Felipe Tovar <f.tovar12@hotmail.com>
2017-07-04 18:08:05 -0500

    Styling email and removed some test

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,6 @@
 class ContactMailer < ApplicationMailer
   def report_email attachment, document_name, email, subject
     attachments[document_name] = { mime_type: Mime[:xlsx], content: attachment }
-    mail(to: email, subject: subject)
+    mail(to: [email.split(",").map(&:strip)], subject: subject)
   end
 end
--- spec/factories/contacts.rb
+++
@@ -1,8 +0,0 @@
-FactoryGirl.define do
-  factory :contact do
-    full_name "MyString"
-    phone "MyString"
-    address "MyString"
-    city "MyString"
-  end
-end
